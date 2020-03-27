745030ecaa7517b764cbfbfadc3295991a37d95b
alihamzaaawan <alihamzaaawan@gmail.com>
2020-03-26 20:21:12 -0700

    move private information into dev.js

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser'); // using for middleware
+const cookieParser = require('cookie-parser'); // using for middleware
+
+const { User } = require('./models/user');
+const config = require('./config/key'); //for security of database URI(in development or production mode)
 mongoose
-  .connect(
-    'mongodb+srv://demo:demo@cluster0-u145r.mongodb.net/test?retryWrites=true&w=majority',
-    {
-      useNewUrlParser: true
-    }
-  )
+  .connect(config.mongoURI, {
+    useNewUrlParser: true
+  })
   .then(() => console.log('DB connected'))
   .catch(err => console.error(err));
 
 app.get('/', (req, res) => {
-  res.send('hello world');
+  res.json({ 'hello ~': 'hi~' });
 });
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+app.post('/api/users/register', (req, res) => {
+  const user = new User(req.bod); // information gained from client
+  user.save((err, userData) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true
+    }); // in case of no error
+  });
+}); // being saved in mongoDB and in case of an error dispalyin it
 
 app.listen(5000);
